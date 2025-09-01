68edb3700f68df076f143716883b57afb0b1aab7
Alamsyah Firdaus <47906520+alamsyahfirdaus@users.noreply.github.com>
2025-09-01 08:36:47 +0700

    Update layout_listview.dart

--- lib/layout_listview.dart
+++ lib/layout_listview.dart
@@ -5,6 +5,27 @@ class LayoutListview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('ListView')),
+      body: ListView(
+        children: [
+          Container(
+            height: 50,
+            color: Colors.amber[600],
+            child: const Center(child: Text('Entry A')),
+          ),
+          Container(
+            height: 50,
+            color: Colors.amber[500],
+            child: const Center(child: Text('Entry B')),
+          ),
+          Container(
+            height: 50,
+            color: Colors.amber[100],
+            child: const Center(child: Text('Entry C')),
+          ),
+        ],
+      ),
+    );
   }
 }
