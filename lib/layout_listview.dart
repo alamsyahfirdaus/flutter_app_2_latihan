281e4a6db574617e1cb61143f0c45d7fd9b58e95
Alamsyah Firdaus <47906520+alamsyahfirdaus@users.noreply.github.com>
2025-09-01 08:33:36 +0700

    Menambahkan File Baru

---
+++ lib/layout_listview.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LayoutListview extends StatelessWidget {
+  const LayoutListview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
